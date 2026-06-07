3c769f5a538be42d4a982a6d217480fd12458aa1
Prajwal <prajwal.personal@proton.me>
2026-06-07 12:18:01 +0530

    Added collision logic

--- game.py
+++ game.py
@@ -11,15 +11,24 @@ class Game:
 
         self.cloud1_img = pygame.image.load("data/images/clouds/cloud_1.png") # loading the backgroung image of cloud
         self.cloud1_img_position = [100,200]
+        self.cloud1_img.set_colorkey((0,0,0)) # replaces black backgroud with transarency
+        self.cloud1_movement = [False,False] # up and down movement of cloud
 
-        self.movement = [False,False]
+        self.collision_area = pygame.Rect(50,50,300,50)
 
     def run(self):
         while(1):
             self.screen.fill((135,206,235)) # fills entire screen with sky blue
 
-            self.cloud1_img_position[1] += self.movement[1] - self.movement[0] # moves the cloud when only 1 key is pressed
+            img_r = pygame.Rect(self.cloud1_img_position[0], self.cloud1_img_position[1], self.cloud1_img.get_width(), self.cloud1_img.get_height())
+            if (img_r.colliderect(self.collision_area)): # detects collision and fills in blue or red accordingly
+                pygame.draw.rect(self.screen, (255,50,100), self.collision_area)
+            else :
+                pygame.draw.rect(self.screen, (0,100,200), self.collision_area)
+            
+            self.cloud1_img_position[1] += self.cloud1_movement[1] - self.cloud1_movement[0] # moves the cloud when only 1 key is pressed
             self.screen.blit(self.cloud1_img,self.cloud1_img_position) # places cloud img at 100px from left and 200 px from right
+
 
             for event in pygame.event.get(): # process all pending event from prev frame one by one
                 if (event.type==pygame.QUIT) :
@@ -28,15 +37,15 @@ class Game:
 
                 if (event.type==pygame.KEYDOWN): # when key is pressed
                     if (event.key == pygame.K_UP):
-                        self.movement[0] = True
+                        self.cloud1_movement[0] = True
                     if (event.key == pygame.K_DOWN):
-                        self.movement[1] = True
+                        self.cloud1_movement[1] = True
 
                 if (event.type==pygame.KEYUP): # when key is released 
                     if (event.key == pygame.K_UP):
-                        self.movement[0] = False
+                        self.cloud1_movement[0] = False
                     if (event.key == pygame.K_DOWN):
-                        self.movement[1] = False
+                        self.cloud1_movement[1] = False
 
             pygame.display.update() # updates the screen
             self.clock.tick(60) # sleep until it achieve 60 FPS 
